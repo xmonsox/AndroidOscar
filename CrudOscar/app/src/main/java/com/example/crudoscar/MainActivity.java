0c815a1515eb7d8bc8449fff80f3ddab260f97be
Kenferinsano <canomoreno78@gmail.com>
2024-02-13 12:09:37 -0500

    Crud Oscar (Create)

---
+++ CrudOscar/app/src/main/java/com/example/crudoscar/MainActivity.java
@@ -0,0 +1,22 @@
+package com.example.crudoscar;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void createView(View vista){
+        Intent intencion = new Intent(getApplicationContext(), Activity_Create.class);
+        startActivity(intencion);
+
+    }
+}
